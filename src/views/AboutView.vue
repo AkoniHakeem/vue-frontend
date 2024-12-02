c1dcc17a001da09322126f1f3380f81b8eebac31
akonihakeem <akozymayor@gmail.com>
2024-12-02 23:17:26 +0100

    added button functionality to about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,25 @@
 <template>
-  <div class="about">
+  <div>
     <h1>This is an about page</h1>
+    <br>
+    <div>
+      <h2>Hmn, this is looking great!</h2>
+    </div>
+    <div>
+      <div>
+        <button style="font-size: larger;" @click="increment">Click Me , and Watch( {{ count }} )</button>
+      </div>
+    </div>
   </div>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue'
+const count = ref(0);
+function increment() {
+  count.value++;
+
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
