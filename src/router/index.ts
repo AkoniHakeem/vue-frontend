2395330335a60370613400c7fc93f5cef633320c
akonihakeem <akozymayor@gmail.com>
2024-12-04 23:05:07 +0100

    created a new view file and added implementations for using template syntax, rawHtml vue directive, v-on vue directive and using function with vue events

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/activity',
+      name: 'activity',
+      component: () => import('../views/ActivityView.vue'),
+    },
   ],
 })
 
---
+++ src/views/ActivityView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <p>This is a template</p>
+        <span>Message: {{ msg }}</span>
+        <p>This is the html message: <span v-bind:id="`color-${colorSuffix}`" v-html="msg"></span> </p>
+        <button v-on:click="toggleColor">Change Text Color</button>
+    </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const msg = '<h3>Welcome<h3/>';
+const colorSuffix = ref('red');
+
+function toggleColor() {
+    colorSuffix.value = colorSuffix.value === 'red' ? 'blue' : 'red';
+}
+
+</script>
+<style lang="css">
+#color-red {
+    color: red;
+}
+
+#color-blue {
+    color: blue
+}
+</style>
